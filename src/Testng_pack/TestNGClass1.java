c0bd48522735fc16e5debc24da96dd4a371701e7
munjalaharikrishna <munjalaharikrishna123@gmail.com>
2023-09-12 15:12:29 +0530

    my files are moving

---
+++ src/Testng_pack/TestNGClass1.java
@@ -0,0 +1,35 @@
+package Testng_pack;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGClass1 {
+  @Test
+  public void ind() {
+	  String name = "India";
+	  Assert.assertEquals(name, "India");
+	  System.out.println("The Given Country is correct as: " +name);
+  }
+  
+  @Test
+  public void aus() {
+	  String name = "Australia";
+	  Assert.assertEquals(name, "Australia");
+	  System.out.println("The Given Country is correct as: " +name);
+  }
+  
+  @Test
+  public void scot() {
+	  String name = "Scotland";
+	  Assert.assertEquals(name, "Scotland");
+	  System.out.println("The Given Country is correct as: " +name);
+  }
+  
+  @Test
+  public void ire() {
+	  String name = "Ireland";
+	  Assert.assertEquals(name, "Ireland");
+	  System.out.println("The Given Country is correct as: " +name);
+  }
+  
+}
